88e8d65f8d7b1066c0164a5bc2a4e9dfa0c1cb92
Aline Dubois <juillet.aline@gmail.com>
2021-03-24 14:59:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const wilders = ["Violaine", "David", "Sarah", "Tom"];
+
+const chalk = require("chalk");
+
+console.log(chalk.yellow(wilders[0]),chalk.green(wilders[1]), chalk.blue(wilders[2]), chalk.red(wilders[3]));
